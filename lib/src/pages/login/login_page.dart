827e4645bec9c6d4b021d6d052ff66b4811e8c21
Frontend <frontend@healonsite.mx>
2022-07-04 00:36:06 -0500

    1. Ceando la pantalla principal (parte 1)

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -6,9 +6,79 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      bottomNavigationBar: SizedBox(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
+      body: Stack(
+        // Posicionar elementos uno encima del otro
+        children: [
+          _backgroundCover(context),
+          Column(
+            // Posicionar elementos uno debajo del otro
+            children: [_imageCover(), _textAppName()],
+          )
+        ],
       ),
     );
   }
+}
+
+// _PRIVATE
+Widget _imageCover() {
+  return SafeArea(
+    child: Container(
+      margin: const EdgeInsets.only(top: 20.0, bottom: 15.0),
+      alignment: Alignment.topCenter,
+      child: Image.asset(
+        'assets/images/delivery.png',
+        height: 130,
+        width: 130,
+      ),
+    ),
+  );
+}
+
+Widget _textAppName() {
+  return const Text(
+    'DELIVERY MYSQL',
+    style: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+  );
+}
+
+Widget _backgroundCover(BuildContext context) {
+  return Container(
+    width: double.infinity,
+    height: MediaQuery.of(context).size.height * 0.4,
+    color: Colors.amber,
+  );
+}
+
+Widget _textDontHaveAccount() {
+  return Row(
+    // Ubicar elementos uno al lado de otro
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: const [
+      Text(
+        '¿No tienes cuenta?',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+        ),
+      ),
+      SizedBox(width: 7),
+      Text(
+        'Registrate Aqui',
+        style: TextStyle(
+          color: Colors.amber,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      ),
+    ],
+  );
 }
